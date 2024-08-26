5f9d9fffd0348d392d54d820cb255a90ac0821ce
Zavier <85642176+ZavierLowe@users.noreply.github.com>
2024-08-26 11:39:26 -0400

    test brand

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-      
+      ff
       </div>
     </main>
   );
